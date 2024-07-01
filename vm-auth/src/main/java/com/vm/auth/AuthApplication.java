418921bdb30e6827fed35783a1e5e005424dacc2
vmchien217 <vmchien217@gmail.com>
2024-07-01 15:32:04 +0700

    update pom

--- vm-auth/src/main/java/com/vm/auth/AuthApplication.java
+++ vm-auth/src/main/java/com/vm/auth/AuthApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZoroApplication {
+public class AuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZoroApplication.class, args);
+		SpringApplication.run(AuthApplication.class, args);
 	}
 }
